4d3056b04f16dd70c1072f85e3f11519e599c53d
zxu598 <zxu598@student.bham.ac.uk>
2026-06-23 15:23:53 +0100

    title done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
 
   return (
     <div style = {{ minHeight: '100vh', backgroundColor: '#ffffff' }}>
+      <h1 style = {{
+        color: '#000000',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        textAlign: 'left',
+        paddingTop: '10px',
+        fontFamily: 'Alte Haas Grotesk',
+        letterSpacing: '-2px'
 
+      }}>Film Emulation</h1>
     </div>
   )
 }
