d2d0675afc84ee59894fadf2cc4541c7fbbee555
zxu598 <zxu598@student.bham.ac.uk>
2026-07-01 13:49:25 +0100

    updated drag and drop behaviour to only accept images and updated interactivity of drag and drop

--- src/fileUploader.jsx
+++ src/fileUploader.jsx
@@ -2,17 +2,46 @@ import {useState} from "react";
 
 function FileUploader(){
     const [files, setFiles] = useState([]);
+    const [isHovered, setIsHovered] = useState(false);
 
+    //this event handler upon a file drop we store the file in droppedFiles then filter it and use setfiles to add it to an array 
     const handleDrop = (e) => {
+        console.log("drop fired");
         e.preventDefault();
         const droppedFiles = Array.from(e.dataTransfer.files);
         const imageFiles = droppedFiles.filter(file => file.type.startsWith("image/"));
+        
+        if (imageFiles.length === 0) {
+            console.log("❌ Upload failed - no valid image files");
+            alert("Only image files are allowed!");
+            setIsDragging(false);
+            return;
+        }
+
+
         setFiles(imageFiles);
+
+        setIsDragging(false);
+
+        console.log("image accepted");
     };
 
-    const handleDrag = (e) => {
+    //this just prevents the default web behaviour when you drag something
+    const handleDragOver = (e) => {
         e.preventDefault();
     };
+
+    const handleUpload = (e) => {
+        console.log("d fired");
+        e.preventDefault();
+        const uploadedFiles = Array.from(e.target.files);
+        const imageFiles = uploadedFiles.filter(file => file.type.startsWith("image/"));
+        setFiles(imageFiles);
+
+
+    };
+    
+
 
     const [isDragging, setIsDragging] = useState(false);
     const handleDragEnter = () => setIsDragging(true);
@@ -22,7 +51,7 @@ function FileUploader(){
         
         <div
             onDrop = {handleDrop}
-            onDrag = {handleDrag}
+            onDragOver = {handleDragOver}
             onDragEnter = {handleDragEnter}
             onDragLeave = {handleDragLeave}
             style = {{
@@ -34,7 +63,7 @@ function FileUploader(){
                 justifyContent: "center",  // centers horizontally
                 flexDirection: "column", 
                 width: "400px",
-                height: "75px",
+                height: "100px",
                 border: "2px dashed #f0f3a2",
                 padding: "20px",
 
@@ -44,12 +73,39 @@ function FileUploader(){
                 backgroundColor: isDragging ? "#3e2723" : "#ffffff" ,
             }}>
 
-           <p>Drag image files here</p>
+           <p
+           style = {{
+            color: "#3e2723"
+           }}
+           >Drag image files here</p>
            <ul>
             {files.map((file, index) => (
                 <li key = {index}>{file.name}</li>
             ))}
             </ul> 
+            <input 
+            type="file" 
+            name="img-upload" 
+            id="img-upload" 
+            style = {{display: "none"}}
+            onChange={handleUpload}/>
+
+            <label
+            htmlFor="img-upload"
+            onMouseEnter = {() => setIsHovered(true)}
+            onMouseLeave = {() => setIsHovered(false)}
+            style = {{
+                color: isDragging ? "#3e2723" : "#ffffff",
+                backgroundColor: isHovered ? "#f0f3a2": "#3e2723",
+                borderRadius: "2px",
+                display: "flex",
+                alignItems: "center",      // centers vertically
+                justifyContent: "center",  // centers horizontally
+                flexDirection: "column",
+                fontFamily: "roboto mono",
+
+            }}
+            > browse </label>
         </div>
     );
 }
